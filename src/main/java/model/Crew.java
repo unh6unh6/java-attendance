6bf9d13e102fed8b735be142de0a365b543de695
min <ruddbs3308@naver.com>
2025-02-18 16:42:13 +0900

    feat(Crew): 출석 확인 기능 구현

---
+++ src/main/java/model/Crew.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Crew {
+
+    private final String nickname;
+
+    private final Map<Integer, LocalDateTime> attendance = new HashMap<>();
+
+    public Crew(final String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Map<Integer, LocalDateTime> getAttendance() {
+        return Collections.unmodifiableMap(attendance);
+    }
+
+    public void doAttendance(final LocalDateTime attendanceTime) {
+        int day = attendanceTime.getDayOfMonth();
+        if (attendance.containsKey(day)) {
+            throw new IllegalArgumentException("[ERROR] 이미 출석했습니다. 수정 기능을 이용해주세요.");
+        }
+        attendance.put(day, attendanceTime);
+    }
+}
---
+++ src/test/java/model/CrewTest.java
@@ -0,0 +1,47 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDateTime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CrewTest {
+
+    @DisplayName("크루 닉네임을 통해 크루를 생성한다")
+    @Test
+    void createCrewTest() {
+        assertThatCode(() -> new Crew("호떡"))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("출석을 한다")
+    @Test
+    void doAttendance() {
+        // Given
+        Crew crew = new Crew("밍트");
+        LocalDateTime attendanceTime = LocalDateTime.of(2024, 12, 3, 9, 0);
+
+        // When
+        crew.doAttendance(attendanceTime);
+
+        // Then
+        Assertions.assertThat(crew.getAttendance()).containsEntry(3, attendanceTime);
+    }
+
+    @DisplayName("이미 출석한 경우 예외가 발생한다")
+    @Test
+    void alreadyAttendanceTest() {
+        // Given
+        Crew crew = new Crew("밍트");
+        LocalDateTime attendanceTime = LocalDateTime.of(2024, 12, 3, 9, 0);
+        crew.doAttendance(attendanceTime);
+
+        // When & Then
+        assertThatThrownBy(() -> crew.doAttendance(attendanceTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 이미 출석했습니다. 수정 기능을 이용해주세요.");
+    }
+}
