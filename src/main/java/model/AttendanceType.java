a722836f4dcdcd0c53f3af719de0518480522dae
min <ruddbs3308@naver.com>
2025-02-19 20:06:20 +0900

    feat(AttendanceType): 출석, 지각, 결석 횟수 계산 기능 구현

--- src/main/java/model/AttendanceType.java
+++ src/main/java/model/AttendanceType.java
@@ -4,11 +4,17 @@ import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 
 public enum AttendanceType {
+
     출석(Duration.ofMinutes(0)),
     지각(Duration.ofMinutes(5)),
     결석(Duration.ofMinutes(30));
+
+    public static final LocalTime INITIAL_TIME = LocalTime.MIN;
 
     AttendanceType(Duration threshold) {
         this.threshold = threshold;
@@ -16,16 +22,27 @@ public enum AttendanceType {
 
     private final Duration threshold;
 
-    public static AttendanceType from(final LocalDateTime time) {
-        LocalTime startTime = getStartTime(time.getDayOfWeek());
-        LocalTime attendanceTime = LocalTime.from(time);
-        if (attendanceTime.isAfter(startTime.plus(결석.threshold))) {
+    public static AttendanceType from(final LocalDateTime attendanceDateTime) {
+        LocalTime startTime = getStartTime(attendanceDateTime.getDayOfWeek());
+        LocalTime attendanceTime = LocalTime.from(attendanceDateTime);
+        if (attendanceTime.equals(INITIAL_TIME) || attendanceTime.isAfter(startTime.plus(결석.threshold))) {
             return 결석;
         }
         if (attendanceTime.isAfter(startTime.plus(지각.threshold))) {
             return 지각;
         }
         return 출석;
+    }
+
+    public static Map<AttendanceType, Integer> countAttendanceType(final List<LocalDateTime> attendanceTimes) {
+        Map<AttendanceType, Integer> result = new EnumMap<>(AttendanceType.class);
+        result.put(출석, 0);
+        result.put(지각, 0);
+        result.put(결석, 0);
+        for (LocalDateTime attendanceTime : attendanceTimes) {
+            result.merge(from(attendanceTime), 1, Integer::sum);
+        }
+        return result;
     }
 
     private static LocalTime getStartTime(DayOfWeek dayOfWeek) {
