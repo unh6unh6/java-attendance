a20c083e52ae77b9a58656609c8fcccc40bae95e
민경윤 <ruddbs3308@naver.com>
2025-02-28 15:15:58 +0900

    feat(InputParser): 입력된 날짜 문자열이 포맷에 어긋난다면 예외를 발생시킨다

--- src/main/java/model/InputParser.java
+++ src/main/java/model/InputParser.java
@@ -1,15 +1,26 @@
 package model;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeParseException;
 
 public class InputParser {
 
-    public LocalTime parseTimeInput(final String input) {
+    public LocalTime parseTime(final String input) {
         try {
             return LocalTime.parse(input, AttendanceDateTimeFormatter.timeFormatter);
         } catch (DateTimeParseException e) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 시간 입력 형식입니다.");
+        }
+    }
+
+    public LocalDate parseDayOfMonth(final String input) {
+        try {
+            int day = Integer.parseInt(input);
+            return LocalDate.of(2024, 12, day);
+        } catch (NumberFormatException | DateTimeException e) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 날짜(일) 입력 형식입니다.");
         }
     }
 }
--- src/test/java/model/InputParserTest.java
+++ src/test/java/model/InputParserTest.java
@@ -3,6 +3,7 @@ package model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.Month;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,15 +22,33 @@ class InputParserTest {
     @DisplayName("시간 문자열을 LocalTime으로 변환한다")
     @Test
     void timeParseTest() {
-        assertThat(inputParser.parseTimeInput("11:50")).hasHour(11).hasMinute(50);
+        assertThat(inputParser.parseTime("11:50")).hasHour(11).hasMinute(50);
     }
 
     @DisplayName("시간 문자열의 포맷이 올바르지 않으면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"11시50분", "11-50", "25:00", "10:60"})
     void wrongTimeFormatParseTest(final String input) {
-        assertThatThrownBy(() -> inputParser.parseTimeInput(input))
+        assertThatThrownBy(() -> inputParser.parseTime(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 올바르지 않은 시간 입력 형식입니다.");
+    }
+
+    @DisplayName("날짜(일)을 2024년 12월의 LocalDate로 변환한다")
+    @Test
+    void dayParseTest() {
+        assertThat(inputParser.parseDayOfMonth("5"))
+                .hasYear(2024)
+                .hasMonth(Month.DECEMBER)
+                .hasDayOfMonth(5);
+    }
+
+    @DisplayName("날짜 문자열의 포맷이 올바르지 않으면 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"5일", "0", "32"})
+    void wrongDayFormatParseTest(final String input) {
+        assertThatThrownBy(() -> inputParser.parseDayOfMonth(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 올바르지 않은 날짜(일) 입력 형식입니다.");
     }
 }
