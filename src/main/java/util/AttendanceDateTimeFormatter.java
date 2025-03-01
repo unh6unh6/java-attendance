be22ffb04fbe3f96ea979b0edef1c822cbac856f
민경윤 <ruddbs3308@naver.com>
2025-03-01 18:25:31 +0900

    feat(AttendanceDateTimeFormatter): LocalDate, LocalTime을 날짜 및 시간 형식 문자열로 변환한다

--- src/main/java/util/AttendanceDateTimeFormatter.java
+++ src/main/java/util/AttendanceDateTimeFormatter.java
@@ -1,8 +1,20 @@
 package util;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class AttendanceDateTimeFormatter {
 
+    public static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM월 dd일 E요일");
     public static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+
+    public static String formatLocalDate(final LocalDate localDate) {
+        return localDate.format(dateFormatter);
+    }
+
+    public static String formatLocalTime(final LocalTime localTime) {
+        return localTime.format(timeFormatter);
+    }
+
 }
