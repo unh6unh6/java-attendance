7bb7a0311485e1ddaa943a9e30fa094ef8a94f66
민경윤 <ruddbs3308@naver.com>
2025-03-02 19:57:10 +0900

    feat(AttendanceHistoryFactory): 원하는 상태의 AttendanceHistory를 얻을 수 있는 팩토리 추가

--- src/test/java/model/AttendanceHistoryTest.java
+++ src/test/java/model/AttendanceHistoryTest.java
@@ -4,9 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -57,25 +58,17 @@ class AttendanceHistoryTest {
         assertThat(history.getHistory()).containsEntry(attendanceDate, attendanceTime);
     }
 
-    //TODO : AttendanceDate, AttendanceTime 생성하는 static method 추가하기
     @DisplayName("출석 기록 및 상태를 날짜순으로 조회한다")
     @Test
     void getHistoryWithAttendanceTypeTest() {
-        AttendanceHistory attendanceHistory = new AttendanceHistory(Map.of(
-                new AttendanceDate(LocalDate.of(2024, 12, 2)),
-                new AttendanceTime(LocalTime.of(13, 0)),
-                new AttendanceDate(LocalDate.of(2024, 12, 3)),
-                new AttendanceTime(LocalTime.of(10, 7)),
-                new AttendanceDate(LocalDate.of(2024, 12, 4)),
-                new AttendanceTime(LocalTime.of(10, 2)),
-                new AttendanceDate(LocalDate.of(2024, 12, 5)),
-                new AttendanceTime(LocalTime.of(10, 6)),
-                new AttendanceDate(LocalDate.of(2024, 12, 6)),
-                new AttendanceTime(LocalTime.of(10, 1)),
-                new AttendanceDate(LocalDate.of(2024, 12, 10)),
-                new AttendanceTime(LocalTime.of(10, 3)),
-                new AttendanceDate(LocalDate.of(2024, 12, 13)),
-                new AttendanceTime(LocalTime.of(10, 2))
+        AttendanceHistory attendanceHistory = AttendanceHistoryFactory.make(Stream.of(
+                LocalDateTime.of(2024, 12, 2, 13, 0),
+                LocalDateTime.of(2024, 12, 3, 10, 7),
+                LocalDateTime.of(2024, 12, 4, 10, 2),
+                LocalDateTime.of(2024, 12, 5, 10, 6),
+                LocalDateTime.of(2024, 12, 6, 10, 1),
+                LocalDateTime.of(2024, 12, 10, 10, 3),
+                LocalDateTime.of(2024, 12, 13, 10, 2)
         ));
 
         List<AttendanceHistoryByDate> expected = List.of(
