a8bbccda07fde40de44ef5db111ac3f998bdc6d6
민경윤 <ruddbs3308@naver.com>
2025-03-05 02:07:34 +0900

    refactor(DismissalCrewSortPolicy): 람다 내부의 변수명을 더욱 명확하게 변경

--- src/main/java/model/policy/DismissalCrewSortPolicy.java
+++ src/main/java/model/policy/DismissalCrewSortPolicy.java
@@ -7,9 +7,10 @@ import dto.DismissalCrewDto;
 import java.util.Comparator;
 
 public enum DismissalCrewSortPolicy {
-    BY_DISMISSAL_TYPE(Comparator.comparingInt(dto -> dto.dismissalType().getPriorityOrder())),
-    BY_LATE_COUNT(Comparator.comparing(dto ->
-            dto.typeCountResult().get(ABSENT) * 3 + dto.typeCountResult().get(LATE), Comparator.reverseOrder())),
+    BY_DISMISSAL_TYPE(Comparator.comparingInt(crewInfo -> crewInfo.dismissalType().getPriorityOrder())),
+    BY_LATE_COUNT(Comparator.comparing(crewInfo ->
+                    crewInfo.typeCountResult().get(ABSENT) * 3 + crewInfo.typeCountResult().get(LATE),
+            Comparator.reverseOrder())),
     BY_NICKNAME(Comparator.comparing(DismissalCrewDto::nickname));
 
     private final Comparator<DismissalCrewDto> comparator;
